e5ab989e0ec212132c7c7b740d5bbed9cae8b521
Aitana <aitana.g.f@opendeusto.es>
2026-03-14 18:06:10 +0100

    creados los DAO para las clases carcel, cuenta y preso en base al ejemplo de alud

---
+++ JailQ/src/main/java/com/example/JailQ/Dao/CarcelDAO.java
@@ -0,0 +1,9 @@
+package com.example.JailQ.Dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.JailQ.Entidades.Carcel;
+
+public interface CarcelDAO extends CrudRepository<Carcel, Integer> {
+
+}
