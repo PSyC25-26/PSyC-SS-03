bd0a408f7640b47cbcfef7d92b495ded7090c2e2
Aitana <aitana.g.f@opendeusto.es>
2026-03-15 13:37:44 +0100

    cambiado a crud repo

--- JailQ/src/main/java/com/example/JailQ/Dao/PresoDAO.java
+++ JailQ/src/main/java/com/example/JailQ/Dao/PresoDAO.java
@@ -4,13 +4,10 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 import com.example.JailQ.Entidades.Preso;
 
-//public interface PresoDAO extends CrudRepository<Preso, Integer> {
-//
-//}
-public interface PresoDAO extends JpaRepository<Preso, Integer> {
 /**
  * Interfaz DAO para la entidad {@link Preso}.
  */
+public interface PresoDAO extends CrudRepository<Preso, Integer> {
 
 
 }
