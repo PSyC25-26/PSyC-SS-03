b1a6890f53968d220ed032de86f587500b27c55e
bluedragon011 <adrian.baz@opendeusto.es>
2026-03-15 13:17:43 +0100

    Implementada funcionalidad de anadirPreso. Fixes #19

--- JailQ/src/main/java/com/example/JailQ/Dao/PresoDAO.java
+++ JailQ/src/main/java/com/example/JailQ/Dao/PresoDAO.java
@@ -1,8 +1,12 @@
 package com.example.JailQ.Dao;
-
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 import com.example.JailQ.Entidades.Preso;
 
-public interface PresoDAO extends CrudRepository<Preso, Integer> {
+//public interface PresoDAO extends CrudRepository<Preso, Integer> {
+//
+//}
+public interface PresoDAO extends JpaRepository<Preso, Integer> {
 
 }
---
+++ JailQ/src/main/java/com/example/JailQ/Service/PresoService.java
@@ -0,0 +1,47 @@
+package com.example.JailQ.Service;
+import java.time.LocalDate;
+
+import com.example.JailQ.Entidades.Preso;
+import com.example.JailQ.Dao.PresoDAO;
+
+public class PresoService {
+    public void anadirPreso(Preso nuevoPreso){
+        //Compronar que existe el objeto
+        if (nuevoPreso == null) {
+            System.err.println("No se ha recibido ningún dato del preso");
+            return;
+        }
+
+        LocalDate fechaActual = LocalDate.now();
+
+        //validación de Nombre y Apellidos (con trim para detectar "" también como vacío)
+        if (nuevoPreso.getNombre() == null || nuevoPreso.getNombre().trim().isEmpty()){
+            System.err.println("El campo nombre es obligatorio");
+        } 
+        else if (nuevoPreso.getApellidos() == null || nuevoPreso.getApellidos().trim().isEmpty()){
+            System.err.println("El campo apellidos es obligatorio");
+        }
+        //validacion de edad
+        else if (nuevoPreso.getFechaNacimiento() == null || 
+                 nuevoPreso.getFechaNacimiento().plusYears(18).isAfter(fechaActual)) {
+            System.err.println("El preso debe ser mayor de edad o la fecha de nacimiento es nula");
+        }
+        //Comprobacion de condena
+        else if (nuevoPreso.getCondena() == null || nuevoPreso.getCondena() <= 0){
+            System.err.println("La condena debe ser mayor a 0 años");
+        }
+        // 4.Validacion de fecha de ingreso -> no puede ser futura y tampoco puede entrar antes de haber nacido
+        else if (nuevoPreso.getFechaIngreso() == null || 
+                 nuevoPreso.getFechaIngreso().isAfter(fechaActual)) {
+            System.err.println("La fecha de ingreso no puede ser futura");
+        }
+        else if (nuevoPreso.getFechaIngreso().isBefore(nuevoPreso.getFechaNacimiento())) {
+            System.err.println("La fecha de ingreso no puede ser anterior al nacimiento");
+        }
+        else {
+            //Si todos los datos son válidos: 
+            System.out.println("Preso validado correctamente. Procediendo a guardar...");
+            PresoDAO.save(nuevoPreso); //Falta implementar funcion save dentro de presoDAo
+        }
+    }
+}
