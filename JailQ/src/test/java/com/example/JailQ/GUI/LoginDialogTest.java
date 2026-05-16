270a2eb75953c4fc6d5a9ceb6ed0f8148e9c40cd
manu.obach <manu.obach@opendeusto.es>
2026-05-16 12:01:24 +0200

    import

--- JailQ/src/test/java/com/example/JailQ/GUI/LoginDialogTest.java
+++ JailQ/src/test/java/com/example/JailQ/GUI/LoginDialogTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.awt.Frame;
 
 public class LoginDialogTest {
 
